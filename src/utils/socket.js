1caa1a9b5b13e94387b834d7bd7e8f7a24ac8e54
Emily Yum <emilyyum@gmail.com>
2020-05-10 07:55:23 -0700

    changed socketioclient

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,4 @@
 import socketIOClient from "socket.io-client";
-const socket = socketIOClient("http://localhost:5000");
+const socket = socketIOClient("https://chat-app-em.herokuapp.com/");
 
 export default socket
